eb3f4e9d0f596909fd49172f375d4f5cc51443fc
Sam Cotterell <Samuel.Cotterell@gmail.com>
2021-08-08 00:52:35 +0100

    sdf

--- index.js
+++ index.js
@@ -1,69 +1,80 @@
 var HID = require('node-hid');
 var mqtt = require('mqtt')
 
-try {
-  var client = mqtt.connect('mqtt://localhost')
+const readSeneye = () => {
+  try {
+    // Open connections 
+    var client = mqtt.connect('mqtt://localhost')
+    client.on('connect', function () {
+      console.log('conneted to MQ')      
+    })
 
-  // Print devices
-  var devices = HID.devices();
-  devices.map(d => { console.log(d) })
+    // Print devices
+    var devices = HID.devices();
+    devices.map(d => { console.log(d) })
 
-  // Connect to Seneye
-  var device = new HID.HID(9463,8708);
+    // Connect to Seneye
+    var device = new HID.HID(9463, 8708);
 
-  // Hello
-  device.write(Buffer.from('HELLOSUD'));
-  device.write(Buffer.from('READING'));
+    // Hello
+    device.write(Buffer.from('HELLOSUD'));
+    device.write(Buffer.from('READING'));
 
 
-  device.on("data", function (data) {
-    var str = '';
-    for (var ii = 0; ii < data.length; ii++) {
-      str += data[ii].toString(16) + ' ';
-    };
+    device.on("data", function (data) {
+      var str = '';
+      for (var ii = 0; ii < data.length; ii++) {
+        str += data[ii].toString(16) + ' ';
+      };
 
-    // Read sensor String
-    if (str.startsWith('0 1')) {
-      const stateBit = data[6]
+      // Read sensor String
+      if (str.startsWith('0 1')) {
+        const stateBit = data[6]
 
-      const phVal = Buffer.from([data[10], data[11]]);
-      const ph = phVal.readUInt16LE() / 100
-      console.log(ph)
+        const phVal = Buffer.from([data[10], data[11]]);
+        const ph = phVal.readUInt16LE() / 100
+        console.log(ph)
 
-      const nh3Val = Buffer.from([data[12], data[13]]);
-      const nh3 = nh3Val.readUInt16LE() / 1000
-      console.log(nh3)
+        const nh3Val = Buffer.from([data[12], data[13]]);
+        const nh3 = nh3Val.readUInt16LE() / 1000
+        console.log(nh3)
 
-      const tempVal = Buffer.from([data[14], data[15], data[16], data[17]]);
-      const temp = tempVal.readUInt32LE() / 1000
-      console.log(temp)
+        const tempVal = Buffer.from([data[14], data[15], data[16], data[17]]);
+        const temp = tempVal.readUInt32LE() / 1000
+        console.log(temp)
 
-      const inWater = isBitOn(stateBit, 2)
-      const noSlide = isBitOn(stateBit, 3)
-      const slideExpired = isBitOn(stateBit, 4)
-      console.log('In Water:', inWater)
-      console.log('No Slide:', noSlide)
-      console.log('Slide Expired:', slideExpired)
+        const inWater = isBitOn(stateBit, 2)
+        const noSlide = isBitOn(stateBit, 3)
+        const slideExpired = isBitOn(stateBit, 4)
+        console.log('In Water:', inWater)
+        console.log('No Slide:', noSlide)
+        console.log('Slide Expired:', slideExpired)
 
-      client.publish('/seneye', JSON.stringify({
+        // MQTT
+        console.log('publish to mq')
+        client.publish('/seneye', JSON.stringify({
+          inWater, noSlide,
+          slideExpired, temp, ph, nh3
 
-        inWater, noSlide,
-        slideExpired, temp, ph, nh3
+        }), () => {
+          console.log("MQ Sent - closing MQ"); 
+          client.end()
+        })
 
-      }))
+        // Close it up
+        console.log('closing serial')
+        device.write(Buffer.from('BYESUD'));
+        device.close()
+      }
+    });
 
-      // Close it up
-      device.write(Buffer.from('BYESUD'));
-    }
-  });
-
-  device.on("error", function (err) {
-    console.log(err)
-  });
-} catch (e) {
-  console.log(e.message)
+    device.on("error", function (err) {
+      console.log(err)
+    });
+  } catch (e) {
+    console.log(e.message)
+  }
 }
-
 function toArray(buffer) {
   var view = [];
   for (var i = 0; i < buffer.length; ++i) {
@@ -76,3 +87,5 @@ function isBitOn(number, index) {
   let binary = number.toString(2);
   return (binary[(binary.length - 1) - index] == "1"); // index backwards
 }
+
+readSeneye();
